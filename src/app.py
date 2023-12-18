a280cf111991b45478a5a514a7a20bd47c814900
Enrique Real Bru <102382251+enriquerb32@users.noreply.github.com>
2023-12-18 22:39:37 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -150,66 +150,65 @@ def render_plot(scipy_col, pyspark_col, i, plot_title, fig_size, data, X, y_test
     fig, ax = plt.subplots(figsize=fig_size)
 
     # Generate the appropriate plot based on the plot group index
-    if i == 0:
-        plt.hist(data['cardio'], edgecolor='black', bins=10, alpha=0.7)
-        ax.set_title('Histogram of Cardio Labels')
-        ax.set_xlabel('Cardio Label')
-        ax.set_ylabel('Frequency')
-    elif i == 1:
-        # Assuming your data is stored in a DataFrame named 'data'
-        categorical_columns = ['gender', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio']
-        
-        # Select only the categorical columns from the DataFrame
-        categorical_data = data[categorical_columns]
-        
-        # Melt the DataFrame to create df_categorical
-        df_categorical = pd.melt(categorical_data)
-        
-        # Now df_categorical is ready to be used in the sns.countplot or other analyses
-        sns.countplot(
-            x="variable", hue="value", data=df_categorical, ax=ax
-        )
-        ax.set_title('Count Plot of Categorical Variables')
-        ax.set_xlabel('Variable')
-        ax.set_ylabel('Count')
-    elif i == 2:
-        # Handle feature importances based on the classifier type
-        if hasattr(model, 'feature_importances_'):
-            feature_importances = model.feature_importances_
-            ax.bar(X.columns, feature_importances)
-            ax.set_title('Feature Importances')
-            ax.set_xlabel('Feature')
-            ax.set_ylabel('Feature Importance')
-    elif i == 3:
-        # ROC/AUC Plot - Execute only for i == 3
-        # Assuming your model has a predict_proba method
-        if hasattr(model, 'predict_proba'):
-            fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X)[:, 1])
-            ax.plot(fpr, tpr)
-            ax.set_title('ROC Curve')
-            ax.set_xlabel('False Positive Rate')
-            ax.set_ylabel('True Positive Rate')
-            st.write('AUC:', round(roc_auc_score(y_test, model.predict_proba(X)[:, 1]), 3))
-    elif i == 4:
-        # Assuming your model has a predict method
-        if hasattr(model, 'predict'):
-            confusion_mat = confusion_matrix(y_test, model.predict(X))
-            sns.heatmap(
-                confusion_mat, annot=True, fmt='g', linewidths=0.5, ax=ax
+    if 0 <= i <= 2:
+        if i == 0:
+            plt.hist(data['cardio'], edgecolor='black', bins=10, alpha=0.7)
+            ax.set_title('Histogram of Cardio Labels')
+            ax.set_xlabel('Cardio Label')
+            ax.set_ylabel('Frequency')
+        elif i == 1:
+            # Assuming your data is stored in a DataFrame named 'data'
+            categorical_columns = ['gender', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio']
+            
+            # Select only the categorical columns from the DataFrame
+            categorical_data = data[categorical_columns]
+            
+            # Melt the DataFrame to create df_categorical
+            df_categorical = pd.melt(categorical_data)
+            
+            # Now df_categorical is ready to be used in the sns.countplot or other analyses
+            sns.countplot(
+                x="variable", hue="value", data=df_categorical, ax=ax
             )
-            ax.set_title('Confusion Matrix')
-            ax.set_xlabel('Predicted Label')
-            ax.set_ylabel('Actual Label')
+            ax.set_title('Count Plot of Categorical Variables')
+            ax.set_xlabel('Variable')
+            ax.set_ylabel('Count')
+        elif i == 2:
+            # Handle feature importances based on the classifier type
+            if hasattr(model, 'feature_importances_'):
+                feature_importances = model.feature_importances_
+                ax.bar(X.columns, feature_importances)
+                ax.set_title('Feature Importances')
+                ax.set_xlabel('Feature')
+                ax.set_ylabel('Feature Importance')
+    elif 3 <= i <= 4:
+        if i == 3:
+            # ROC/AUC Plot - Execute only for i == 3
+            # Assuming your model has a predict_proba method
+            if hasattr(model, 'predict_proba'):
+                fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X)[:, 1])
+                ax.plot(fpr, tpr)
+                ax.set_title('ROC Curve')
+                ax.set_xlabel('False Positive Rate')
+                ax.set_ylabel('True Positive Rate')
+                st.write('AUC:', round(roc_auc_score(y_test, model.predict_proba(X)[:, 1]), 3))
+        elif i == 4:
+            # Assuming your model has a predict method
+            if hasattr(model, 'predict'):
+                confusion_mat = confusion_matrix(y_test, model.predict(X))
+                sns.heatmap(
+                    confusion_mat, annot=True, fmt='g', linewidths=0.5, ax=ax
+                )
+                ax.set_title('Confusion Matrix')
+                ax.set_xlabel('Predicted Label')
+                ax.set_ylabel('Actual Label')
 
     # Display the plot in the appropriate column
-    if i < 3:
-        scipy_col.pyplot(fig)
-    else:
-        pyspark_col.pyplot(fig)
+    scipy_col.pyplot(fig)
 
     # Return the figure and axes
     return fig, ax
-
+    
 
 # Load data and prepare dataset
 data = scikit_func.load_data()
@@ -223,7 +222,6 @@ Selecting _Yes_ will trigger Spark Session automatically!
 pyspark_enabled = st.sidebar.radio("PySpark_Enabled", ('No', 'Yes'))
 
 
-# Main function
 def main():
     st.title('''Streamlit ![](https://assets.website-files.com/5dc3b47ddc6c0c2a1af74ad0/5e0a328bedb754beb8a973f9_logomark_website.png) Healthcare ML Data App''')
     st.subheader('Streamlit Healthcare example By [Enrique Real](https://es.linkedin.com/in/enrique-real-bru-057150237)')
@@ -254,28 +252,33 @@ def main():
               ![pyspark](https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Apache_Spark_logo.svg/120px-Apache_Spark_logo.svg.png)
               ''')
 
-        # Dynamically create columns for the Scikit-Learn plots
-        for i in range(3):
-            col_plot = scikit_col.columns(2)
+        # Render the general plots for PySpark
+        for i in range(3):  # Assuming there are 3 general plots
+            fig, ax = render_plot(scikit_col, pyspark_col, i, f'General Plot {i + 1}', (8, 6), data, X_test, y_test, None)
+            plots.append((fig, ax))
+
+        # Dynamically create columns for the PySpark plots
+        for i in range(2):  # Assuming there are 2 PySpark-specific plots
+            col_plot = pyspark_col.columns(2)
             with col_plot[0], _lock:
-                st.subheader(f'Scikit-Learn Plot {i + 1}')
+                st.subheader(f'PySpark Plot {i + 1}')
             with col_plot[1]:
                 # Render the corresponding plot within the column
-                classifier_name = scikit_col.selectbox(
+                classifier_name = pyspark_col.selectbox(
                     'Select classifier',
-                    scikit_func.get_sidebar_classifier(),
-                    key='scikit'
+                    pyspark_func.get_sidebar_classifier(),
+                    key='pyspark'
                 )
                 params = add_parameter_ui(classifier_name)
-                scikit_col.write(f'Classifier = {classifier_name}')
-                scikit_col.write(f'Accuracy = {scikit_func.trigger_classifier(classifier_name, params, X_train, X_test, y_train, y_test)}')
+                pyspark_col.write(f'Classifier = {classifier_name}')
+                pyspark_col.write(f'Accuracy = {pyspark_func.trigger_classifier(classifier_name, params, X_train, X_test, y_train, y_test)}')
 
-                # Instantiate the Scikit-learn model based on the selected classifier
-                model = scikit_func.get_model(classifier_name, params)
+                # Instantiate the PySpark model based on the selected classifier
+                model = pyspark_func.get_model(classifier_name, params)
 
                 # Render the plots using the render_plot function
-                for j in range(3):  # Assuming there are 3 plots
-                    fig, ax = render_plot(scikit_col, pyspark_col, j, f'Scikit-Learn Plot {j + 1}', (8, 6), data, X_test, y_test, model)
+                for j in range(2):  # Assuming there are 2 PySpark-specific plots
+                    fig, ax = render_plot(scikit_col, pyspark_col, j + 3, f'PySpark Plot {j + 1}', (8, 6), data, X_test, y_test, model)
                     plots.append((fig, ax))
 
         # Display the plots for PySpark
@@ -288,8 +291,13 @@ def main():
             __Scikit Learn ML__
             ![scikit](https://scikit-learn.org/stable/_static/scikit-learn-logo-small.png)''')
 
+        # Render the general plots for Scikit-Learn
+        for i in range(3):  # Assuming there are 3 general plots
+            fig, ax = render_plot(scikit_col, pyspark_col, i, f'General Plot {i + 1}', (8, 6), data, X_test, y_test, None)
+            plots.append((fig, ax))
+
         # Dynamically create columns for the Scikit-Learn plots
-        for i in range(3):
+        for i in range(2):  # Assuming there are 2 Scikit-Learn-specific plots
             col_plot = scikit_col.columns(2)
             plots = []  # Store the plots
             with col_plot[0], _lock:
@@ -309,8 +317,8 @@ def main():
                 model = scikit_func.get_model(classifier_name, params)
 
                 # Render the plots using the render_plot function
-                for j in range(3):  # Assuming there are 3 plots
-                    fig, ax = render_plot(scikit_col, pyspark_col, j, f'Scikit-Learn Plot {j + 1}', (8, 6), data, X_test, y_test, model)
+                for j in range(2):  # Assuming there are 2 Scikit-Learn-specific plots
+                    fig, ax = render_plot(scikit_col, pyspark_col, j + 3, f'Scikit-Learn Plot {j + 1}', (8, 6), data, X_test, y_test, model)
                     plots.append((fig, ax))
 
                 # Display the plots
@@ -322,4 +330,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
